acd9faa30494b8069141c3f2d957ab99ac39d931
airtonferreira <airton.felix@dce.ufpb.br>
2019-09-22 11:30:46 -0300

    Oportunidades Model

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -1,9 +1,14 @@
 package com.algaworks.comercial.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/oportunidades")
 public class OportunidadeController {
+	
+	@GetMapping
 	public String Listar() {
 		return "Hello!";
 	}
