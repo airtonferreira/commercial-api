04cca1782196a89f44c93a12473929a31d2eef61
airtonferreira <airton.felix@dce.ufpb.br>
2019-09-22 13:06:48 -0300

    Mysql Connection and more

---
+++ src/main/resources/db/migration/V01__nova-tabela-oportunidade.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Oportunidade (
+	id bigint auto_increment not null,
+	nome_prospecto varchar(80) not null,
+	descricao varchar(200) not null,
+	valor decimal(10, 2),
+	
+	primary key(id)
+
+);
