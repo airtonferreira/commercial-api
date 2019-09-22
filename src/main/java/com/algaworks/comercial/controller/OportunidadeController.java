38568b31cfc358cd1863b5c4ea51ae7adb0cc476
airtonferreira <airton.felix@dce.ufpb.br>
2019-09-22 10:48:05 -0300

    Initial commit

---
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -0,0 +1,11 @@
+package com.algaworks.comercial.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OportunidadeController {
+	public String Listar() {
+		return "Hello!";
+	}
+
+}
